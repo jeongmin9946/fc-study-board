6c42685f2fbdb5a123aa30c94b4caa735da6908f
TreeName <jeongmin9946@gmail.com>
2023-11-03 16:38:41 +0900

    #6 - Spring_Boot 시작
    스프링 부트 프로젝트 초기화
    들어간 초기화 기술
     * SpringWeb
     * Acturator
     * DevTools
     * Lombok

---
+++ src/test/java/com/treename/board/FcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treename.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
