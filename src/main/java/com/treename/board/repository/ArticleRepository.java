938d54535796c898908df0f1f02bbe45d8164bb0
TreeName <jeongmin9946@gmail.com>
2023-11-06 22:30:00 +0900

    #5 - Data Rest 설정
    게시글, 댓글의 json api를 자동으로 restful하게 만들게 설정.

--- src/main/java/com/treename/board/repository/ArticleRepository.java
+++ src/main/java/com/treename/board/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.treename.board.repository;
 
 import com.treename.board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
